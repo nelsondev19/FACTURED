048477181bc0ff9377206a9312973a50aca57d08
nelsondev19 <nelsonher019@gmail.com>
2025-01-11 14:34:31 -0600

    [FIX] fix function change status

--- frontend/components/Task/index.tsx
+++ frontend/components/Task/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const endpoint = `${process.env.NEXT_PUBLIC_BOARD_API}`;
 
-function Task({ task, TaskSelected, setTaskSelected }: Props) {
+function Task({ task }: Props) {
   const [CurrentStatus, setCurrentStatus] = useState(task.status);
 
   const handleDragStart = (e: DragEvent<HTMLDivElement>) => {
@@ -21,36 +21,32 @@ function Task({ task, TaskSelected, setTaskSelected }: Props) {
     setTimeout(() => {
       target.style.display = "none";
     }, 0);
-
-    setTaskSelected(task);
   };
 
   const changeStatus = async (e: ChangeEvent<HTMLSelectElement>) => {
     const status = e.currentTarget.value;
 
-    if (TaskSelected !== null) {
-      try {
-        const TaskSelectedCopy = TaskSelected;
+    try {
+      const TaskSelectedCopy = task;
 
-        TaskSelectedCopy.status = status;
-        setCurrentStatus(status);
+      TaskSelectedCopy.status = status;
+      setCurrentStatus(status);
 
-        const res = await (
-          await fetch(`${endpoint}/tasks/update`, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(TaskSelectedCopy),
-          })
-        ).json();
+      const res = await (
+        await fetch(`${endpoint}/tasks/update`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(TaskSelectedCopy),
+        })
+      ).json();
 
-        if (res.message === "success") {
-          console.log("Status from task updated! ID ->", task.id);
-        }
-      } catch (error) {
-        console.log(error);
+      if (res.message === "success") {
+        console.log("Status from task updated! ID ->", task.id);
       }
+    } catch (error) {
+      console.log(error);
     }
   };
 
